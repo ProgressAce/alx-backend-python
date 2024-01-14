82bba5dff2836c34de034a4d0ca5eb090d5a4beb
ProgressAce <daiyaansallie7@gmail.com>
2024-01-14 10:10:37 +0200

    make more detailed type annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of a string and the square of v as a float.
 
     Args:
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Annotates the below function the appropriate types."""
 
-from typing import Iterator, Sequence, List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns each element's length and value in tuple form.
 
     Arg:
